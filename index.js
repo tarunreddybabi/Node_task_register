9e9ee2e8c5731614376238456e905ff4e47054dc
tarunreddybabi <tarunreddybabi@gmail.com>
2024-02-16 18:13:42 +0530

    node_task_register

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { LocalStorage } = require("node-localstorage");
+const app = express();
+
+app.use(express.json());
+
+const localStorage = new LocalStorage("./start");
+
+app.post("/register", (req, res) => {
+  const newUser = req.body;
+  if (!newUser || !newUser.username || !newUser.email) {
+    res
+      .status(400)
+      .json({ error: "Invalid user data" });
+    return;
+  }
+
+  let users = localStorage.getItem("users");
+  users = users ? JSON.parse(users) : [];
+
+  users.push(newUser);
+
+  localStorage.setItem("users", JSON.stringify(users));
+  res.json({ message: "User Registered Successfully" });
+});
+
+app.listen(2001, () => {
+  console.log("Server running Successfully");
+});
